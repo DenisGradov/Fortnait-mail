ddfe18ceb6b7d3d6d0f14fd55356606014626069
MSPHelperSamp <155314413+MSPHelperSamp@users.noreply.github.com>
2024-05-04 11:20:37 +0200

    добавил helloworld в беке, что бы тестить бек

--- back/app.js
+++ back/app.js
@@ -236,6 +236,23 @@ app.post("/api/checkPost", (req, res) => {
   }
 });
 
+app.get("/helloworld", (req, res) => {
+  res.status(200);
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Hello World</h1>
+    </body>
+    </html>
+  `);
+});
+
 app.post("/api/login", (req, res) => {
   console.log("sssss2");
   const { formData } = req.body;
